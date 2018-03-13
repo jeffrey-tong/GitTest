d4de9a47588e470040cbe8bc745386391d794d2f
DESKTOP-I8RNRT3\Jeffrey <jeffrey.tong@ucalgary.ca>
2018-03-13 09:58:34 -0600

    added "Branch Test 2!"

--- Git/src/Main.java
+++ Git/src/Main.java
@@ -1,11 +1,7 @@
 
 public class Main {
 	This is code?
-<<<<<<< HEAD
-	This is for branch Test2?
-=======
-	This is for branch Test2!
+	Branch Test 2!
 }
->>>>>>> 0611f557f76f4856e4508f8c5038aceb5c17a877
 
 
