a067a30d5acc26946baa8cfad4c0953bd4e27527
DESKTOP-I8RNRT3\Jeffrey <jeffrey.tong@ucalgary.ca>
2018-03-13 09:50:13 -0600

    no message

--- Git/src/Main.java
+++ Git/src/Main.java
@@ -1,11 +1,6 @@
 
 public class Main {
 	This is code?
-	This is for branch Test2
-<<<<<<< HEAD
-	I dont want a conflict??
+	This is for branch Test2?
 
-=======
-	I want a conflict!
->>>>>>> fa49712b3aa3c5c78251f7a71afd07b6ed70680b
 
